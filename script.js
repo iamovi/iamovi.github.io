11e9a3cde3c6cc3f98893eaf6a0957252a584458
Maruf Ovi <imgoinin.iamovi@gmail.com>
2025-12-30 10:53:15 +0600

    update img setting

--- script.js
+++ script.js
@@ -5,3 +5,24 @@
       });
       document.getElementById(sectionId).style.display = 'block'; // show selected
     }
+    
+const modal = document.getElementById('imageModal');
+const modalImg = document.getElementById('modalImage');
+const closeBtn = document.querySelector('.close-btn');
+
+function openImage(src) {
+  modal.style.display = 'flex';
+  modalImg.src = src;
+}
+
+closeBtn.onclick = () => {
+  modal.style.display = 'none';
+};
+
+// close when clicking outside image
+modal.onclick = (e) => {
+  if (e.target === modal) {
+    modal.style.display = 'none';
+  }
+};
+
